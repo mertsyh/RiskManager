e65b4fbb67cf09acecd869368fef7f332a4598d9
mertsyh <2300007247@stu.iku.edu.tr>
2026-06-08 21:25:57 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/RiskManager/' // Bunu ekledik. Başındaki ve sonundaki eğik çizgilere dikkat et.
 })
